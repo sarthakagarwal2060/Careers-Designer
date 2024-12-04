dd79b1e48c6f7292f7a2ec03e9d7b35eadc7181a
Yuji25 <buntysoni2529@gmail.com>
2024-12-05 00:14:50 +0530

    Another major changes in job categories, some css

--- searchScript.js
+++ searchScript.js
@@ -2,31 +2,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const jobSearchForm = document.getElementById('job-search-form');
     const skillsInput = document.getElementById('skills-input');
     const locationInput = document.getElementById('location-input');
-
-    // Resume upload functionality
-    const resumeInput = document.getElementById('resume');
-    const fileNameSpan = document.getElementById('file-name');
-    const checkmark = document.getElementById('checkmark');
-
-    // Display uploaded file name
-    window.displayFileName = function() {
-        if (resumeInput.files.length > 0) {
-            fileNameSpan.textContent = resumeInput.files[0].name;
-            checkmark.style.display = 'inline';
-        } else {
-            fileNameSpan.textContent = 'No file chosen';
-            checkmark.style.display = 'none';
-        }
-    }
+    const viewJobLinks = document.querySelectorAll('.view-jobs');
 
     // Handle job search form submission
     jobSearchForm.addEventListener('submit', (e) => {
         e.preventDefault();
-        
+
         const skills = skillsInput.value;
         const location = locationInput.value;
-    
+
         // Redirect to search results page with query parameters
         window.location.href = `searchResults.htm?query=${encodeURIComponent(skills)}&location=${encodeURIComponent(location)}`;
+    });
+
+    // Add a click event listener to each link
+    viewJobLinks.forEach(link => {
+        link.addEventListener('click', event => {
+            // Prevent the default link behavior
+            event.preventDefault();
+
+            // Find the nearest <h3> tag relative to the clicked link
+            const categoryHeading = link.closest('.category').querySelector('h3');
+
+            // Get the text content of the <h3> tag
+            const categoryText = categoryHeading.textContent;
+
+            window.location.href = `jobCategoriesResults.htm?query=${encodeURIComponent(categoryText)}`;
+        });
     });
 });
